97eb56dfbf3ecffa01d0430f7aaf13a4303994e7
Ed Angelis <ed4ngelis@gmail.com>
2019-10-12 05:55:54 -0300

    Thrust sphere

--- Assets/scripts/ballController.cs
+++ Assets/scripts/ballController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class ballController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float thust;
+    private Rigidbody sphereBody;
     void Start()
     {
-        
+        sphereBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetKeyDown(KeyCode.Space)){
+            sphereBody.isKinematic = false ;
+
+            sphereBody.velocity = new Vector3( 0, 0 );
+            sphereBody.AddForce( new Vector3( 0, thust ), ForceMode.Impulse);
+
+        }
     }
 }
